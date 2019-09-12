f067e2f7ca7785144ef7b74ddb248b72f037104d
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2019-09-11 22:53:32 -0400

    Starting with lists

--- App.js
+++ App.js
@@ -2,19 +2,22 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponentsScreen from "./src/screens/ComponentsScreen";
 import ExerciseComponent from "./src/screens/ExerciseComponent";
+import ListScreen from "./src/screens/ListScreen";
 
-const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-    Components: ComponentsScreen,
-    Exercise: ExerciseComponent
-  },
-  {
-    initialRouteName: 'Exercise',
-    defaultNavigationOptions: {
-      title: 'App'
+let navigator;
+navigator = createStackNavigator(
+    {
+        Home: HomeScreen,
+        Components: ComponentsScreen,
+        Exercise: ExerciseComponent,
+        List: ListScreen
+    },
+    {
+        initialRouteName: 'List',
+        defaultNavigationOptions: {
+            title: 'App'
+        }
     }
-  }
 );
 
 export default createAppContainer(navigator);
--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const ListScreen = () => {
-
+    return (
+        <View>
+            <Text>List Screen</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({

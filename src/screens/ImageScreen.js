8dad3a11b94a8b52bf99280f32590f8a2b8975e4
Alejandro Hernández <alejandrehl@icloud.com>
2019-10-06 10:15:25 -0400

    Image detail

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,2 +1,10 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+
+const ImageScreen = () => {
+  return <Text>Image Screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageScreen;

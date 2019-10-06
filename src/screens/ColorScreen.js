16084fc177ed903fcd67fe412601df6365cf5090
Alejandro Hernández <alejandrehl@icloud.com>
2019-10-06 15:23:09 -0400

    generando squares con colores aleatorios. UseState Hook

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,11 +1,33 @@
-import React from "react";
-import { View, StyleSheet, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Button, FlatList } from "react-native";
 
 const ColorScreen = () => {
+  const [colors, setColors] = useState([]);
+
   return (
     <View>
-      <Button title="Add a Color" />
-      <View style={styles.view} />
+      <Button
+        title="Add a Color"
+        onPress={() => {
+          setColors([...colors, randomRgb()]);
+        }}
+      />
+
+      <FlatList
+        keyExtractor={item => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                backgroundColor: item
+              }}
+            />
+          );
+        }}
+      />
     </View>
   );
 };
@@ -18,12 +40,6 @@ const randomRgb = () => {
   return `rgb(${red}, ${green}, ${blue})`;
 };
 
-const styles = StyleSheet.create({
-  view: {
-    height: 100,
-    width: 100,
-    backgroundColor: `${randomRgb()}`
-  }
-});
+const styles = StyleSheet.create({});
 
 export default ColorScreen;
